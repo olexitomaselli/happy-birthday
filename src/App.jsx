d86d2396f1130cb56cc05c7153712f962632c6e3
olexitomaselli <olexitom29@gmail.com>
2024-04-22 16:54:08 +0200

    Created all components and data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
 import "./App.css";
+import data from "./data";
+import { List } from "./Components/List/List";
 
 function App() {
+  let [people, setPeople] = useState(data);
+
   return (
     <>
-      <h1>Hi I'm a new app</h1>
+      <main>
+        <h1>{people.length} Birth Today</h1>
+        <List people={people} />
+        <button type="button" onClick={() => setPeople([])}>
+          Clear All
+        </button>
+      </main>
     </>
   );
 }
---
+++ src/Components/People/Person.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Person = ({ image, name, age }) => {
+  return (
+    <article>
+      <img src={image} alt={name} />
+      <div>
+        <h4>{name}</h4>
+        <p>{age} years</p>
+      </div>
+    </article>
+  );
+};
